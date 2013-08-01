2cd3959be2482300c9abfa070e8bd676ae2a8b1a
MakerSquare Student <students@themakersquare.com>
2013-08-01 09:56:37 -0500

    number interact

--- bowling_game.rb
+++ bowling_game.rb
@@ -1,5 +1,18 @@
 class BowlingGame
+
   def initialize 
     @rolls = []
   end
+
+# Records rolls.
+# pins = # of pins knocked down this roll
+  def roll(pins)
+    @rolls.push(pins)
+  end
+
+# Returns integer score for the game
+  def score
+    @rolls.reduce(:+)
+  end
+
 end
