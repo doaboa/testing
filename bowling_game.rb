84fb67d23214a7631c4969049917b92f4bc99baf
MakerSquare Student <students@themakersquare.com>
2013-08-01 10:56:02 -0500

    basic func. need to refactor score method

--- bowling_game.rb
+++ bowling_game.rb
@@ -10,25 +10,37 @@ class BowlingGame
     @rolls.push(pins)
   end
 
+
 # Returns integer score for the game
   def score
     total_score = 0
     current_roll = 0
 
-    while current_roll < @rolls.size -1
+    while current_roll < @rolls.size - 1
       roll      = @rolls[current_roll]
       next_roll = @rolls[current_roll + 1]
 
-      if roll + next_roll == 10
+      if roll == 10
+        total_score += 10 + @rolls[current_roll + 1] + @rolls[current_roll + 2]
+        current_roll += 1
+      elsif roll + next_roll == 10
         total_score += 10 + @rolls[current_roll + 2]
+        current_roll += 2
       else
         total_score += roll + next_roll
+        current_roll += 2
       end
 
-      current_roll += 2
     end
 
     return total_score
   end
+
+  private
+
+  def strike
+    
+  end
+
 
 end
